7fa1ae4e8844da1d236fbb4de7217aebe9b615e8
ctcl-bregis <ctcl@CTCL-WBPC5B>
2022-11-29 23:37:11 -0500

    Something? Does not compile yet.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
-use std::net::TcpListener;
-use ctclsite::start;
+use actix_web::{App, web, get, error, Error, HttpResponse, HttpServer};
+use tera::{Tera, Context};
+use once_cell::sync::OnceCell;
+use once_cell::sync::Lazy;
+
+pub static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
+    let mut tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Template parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    tera.autoescape_on(vec!["html", ".sql"]);
+    tera
+});
+
+#[get("/")]
+async fn root() -> Result<HttpResponse, Error> {
+    let mut context = Context::new();
+    let title = String::from("CTCL Website");
+    context.insert("title", &title);
+    match TEMPLATES.render("main_content.html", &context) {
+        Ok(body) => Ok(HttpResponse::Ok().body(body)),
+        Err(err) => {
+            eprintln!("## Tera error: {}", err);
+            Err(error::ErrorInternalServerError(err))
+        },
+    }
+}
+
+#[get("/")]
+async fn root() -> Result<HttpResponse, Error> {
+    let mut context = Context::new();
+    let title = String::from("CTCL Website");
+    context.insert("title", &title);
+    match TEMPLATES.render("main_content.html", &context) {
+        Ok(body) => Ok(HttpResponse::Ok().body(body)),
+        Err(err) => {
+            eprintln!("## Tera error: {}", err);
+            Err(error::ErrorInternalServerError(err))
+        },
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	std::env::set_var("RUST_LOG", "actix_web=info");
-	env_logger::init();
-	
-	let listener = TcpListener::bind("127.0.0.1:5000")?;
-	start_blog(listener)?.await?;
-	Ok(())
+    HttpServer::new(|| {
+        App::new().service(root)
+    })
+    .bind("127.0.0.1:5000")?
+    .run()
+    .await
 }
+        
+            
