e5450456678f0d2d4c5773ac0cbd403c7b1f0171
ctcl-bregis <ctcl@CTCL-GAMEPC3>
2022-11-30 15:50:34 -0500

    Routing?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use actix_web::{App, web, get, error, Error, HttpResponse, HttpServer};
+use actix_web::{App, web, get, error, Error, HttpResponse, HttpServer, Responder};
 use tera::{Tera, Context};
-use once_cell::sync::OnceCell;
 use once_cell::sync::Lazy;
 
 pub static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
@@ -11,14 +10,14 @@ pub static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
             ::std::process::exit(1);
         }
     };
-    tera.autoescape_on(vec!["html", ".sql"]);
+    tera.autoescape_on(vec![".html", ".sql"]);
     tera
 });
 
 #[get("/")]
-async fn root() -> Result<HttpResponse, Error> {
+async fn root() -> impl Responder {
     let mut context = Context::new();
-    let title = String::from("CTCL Website");
+    let title = String::from("CrazyblocksTechnologies Computer Laboratories - Welcome");
     context.insert("title", &title);
     match TEMPLATES.render("main_content.html", &context) {
         Ok(body) => Ok(HttpResponse::Ok().body(body)),
@@ -29,14 +28,27 @@ async fn root() -> Result<HttpResponse, Error> {
     }
 }
 
+async fn rl_main() -> impl Responder {
+    let mut context = Context::new();
+    let title = String::from("RAMList - Menu");
+    context.insert("title", &title);
+    match TEMPLATES.render("ramlist_menu.html", &context) {
+        Ok(body) => Ok(HttpResponse::Ok().body(body)),
+        Err(err) => {
+            eprintln!("## Tera error: {}", err);
+            Err(error::ErrorInternalServerError(err))
+        }, 
+    }
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().service(root)
+            .route("/ramlist/", web::get().to(rl_main))
     })
     .bind("127.0.0.1:5000")?
     .run()
     .await
-}
-        
-            
+} 
