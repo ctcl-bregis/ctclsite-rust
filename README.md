2a038c00754be9c2fe937fbf17a8e46663f0a8c4
ctcl-bregis <ctcl@CTCL-MWDL2>
2022-11-30 23:53:19 -0500

    Routes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ctclsite-rust
 
 This is the upcoming Rust version of the [CrazyblocksTechnologies Computer Laboratories website](crazyblockstech.com) planned to replace the current Python-based version.
+
+I am absolutely new to Rust at the moment and this project is my first experience with the programming language.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use actix_web::{App, web, get, error, Error, HttpResponse, HttpServer, Responder};
 use tera::{Tera, Context};
 use once_cell::sync::Lazy;
+extern crate comrak;
+use comrak::{parse_document, format_html, Arena, ComrakOptions};
+use comrak::nodes::{AstNode, NodeValue};
+
 
 pub static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
     let mut tera = match Tera::new("templates/**/*") {
@@ -17,7 +21,7 @@ pub static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
 #[get("/")]
 async fn root() -> impl Responder {
     let mut context = Context::new();
-    let title = String::from("CrazyblocksTechnologies Computer Laboratories - Welcome");
+    let title = String::from("Welcome - CrazyblocksTechnologies Computer Laboratories");
     context.insert("title", &title);
     match TEMPLATES.render("main_content.html", &context) {
         Ok(body) => Ok(HttpResponse::Ok().body(body)),
@@ -30,7 +34,7 @@ async fn root() -> impl Responder {
 
 async fn rl_main() -> impl Responder {
     let mut context = Context::new();
-    let title = String::from("RAMList - Menu");
+    let title = String::from("RAMList Menu - CrazyblocksTechnologies Computer Laboratories");
     context.insert("title", &title);
     match TEMPLATES.render("ramlist_menu.html", &context) {
         Ok(body) => Ok(HttpResponse::Ok().body(body)),
@@ -42,11 +46,40 @@ async fn rl_main() -> impl Responder {
 }
 
 
+async fn blog_main() -> impl Responder {
+    let mut context = Context::new();
+    let title = String::from("Blog Posts - CrazyblocksTechnologies Computer Laboratories");
+    context.insert("title", &title);
+    match TEMPLATES.render("main_blog_menu.html", &context) {
+        Ok(body) => Ok(HttpResponse::Ok().body(body)),
+        Err(err) => {
+            eprintln!("## Tera error: {}", err);
+            Err(error::ErrorInternalServerError(err))
+        }, 
+    }
+}
+
+async fn blog_post() -> impl Responder {
+    let mut context = Context::new();
+    let title = String::from("Blog Posts - CrazyblocksTechnologies Computer Laboratories");
+    context.insert("title", &title);
+    match TEMPLATES.render("main_blog_post.html", &context) {
+        Ok(body) => Ok(HttpResponse::Ok().body(body)),
+        Err(err) => {
+            eprintln!("## Tera error: {}", err);
+            Err(error::ErrorInternalServerError(err))
+        }, 
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().service(root)
+            // TODO: figure out how to redirect to page with / at the end
             .route("/ramlist/", web::get().to(rl_main))
+            .route("/blog/", web::get().to(blog_main))
+            .route("/blog/{post}/", web::get().to(blog_post))
     })
     .bind("127.0.0.1:5000")?
     .run()
