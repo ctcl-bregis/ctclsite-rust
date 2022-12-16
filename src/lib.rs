8007c3c8c9adfcad9e4bc211baef6ebd50a92afd
ctcl-bregis <brayden.regis@gmail.com>
2022-12-15 22:40:34 -0500

    eplace use of HashMap with BTreeMap to have inserted items preserve order.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use csv::{self, Error};
 //use serde::{Deserialize, Deserializer};
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fs;
 
 use comrak::{markdown_to_html, ComrakOptions};
 
-type Record = HashMap<String, String>;
+type Record = BTreeMap<String, String>;
 
-pub fn csv2hm(path: &str) -> Result<Vec<HashMap<String, String>>, Error> {
+pub fn csv2hm(path: &str) -> Result<Vec<BTreeMap<String, String>>, Error> {
     let mut records = Vec::new();
     let rdr = csv::Reader::from_path(path);
     
@@ -20,7 +20,7 @@ pub fn csv2hm(path: &str) -> Result<Vec<HashMap<String, String>>, Error> {
 
 pub fn md2html(path: &str) -> Result<String, Error> {
     let md = fs::read_to_string(path)
-        .expect("Should have been able to read the file");
+        .expect("File read error");
         
     let content = markdown_to_html(&md, &ComrakOptions::default());
     
