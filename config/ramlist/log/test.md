d90424b09d87a150cb78998174580a21afa4c411
ctcl-bregis <brayden.regis@gmail.com>
2023-02-03 12:46:54 -0500

    Revert "Total removal of "RAMList" and its contents"
    This reverts commit 12b8048665e41f28856b5924bdce1c04b1e873e7.

---
+++ config/ramlist/log/test.md
@@ -0,0 +1,13 @@
+RAMList gained major updates with the total rewrite of the CTCL website.
+
+**What was changed:**
+
+- "About RAMList" page added.
+- Each list has its own "Last Updated" date.
+- "Back to menu" link on each list.
+- Performance improvements.
+- DDR5, Mobile Device, Hard Drive lists added with some additions to each list.
+- Table widths fixed and now each column has a defined width.
+- Markdown syntax for About and Announcement pages.
+- Updated footer
+- Fixed horizontal rule length on list pages
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 // Purpose: Main program code
-// Date: November 28, 2022 - February 1, 2023
+// Date: November 28, 2022 - January 1, 2023
 // CrazyblocksTechnologies Computer Laboratories, Brayden Regis - 2022-2023
 use std::{error::Error, collections::BTreeMap};
 use actix_web::{App, web, get, error, HttpResponse, HttpServer, http::StatusCode, Responder, web::Path};
 use actix_files as fs;
 use tera::Tera;
 use once_cell::sync::Lazy;
-use ctclsite::{csv2im, md2html, mkcontext};
+use ctclsite::{csv2im, md2html, mkcontext, rl_list_gen};
 #[macro_use] extern crate serde_derive;
 
 #[derive(Deserialize)]
@@ -81,6 +81,125 @@ async fn privacy() -> impl Responder {
         }, 
     }
 }
+
+// RAMList main menu
+async fn rl_main() -> impl Responder {
+    let mut context = mkcontext("ramlist", "root").unwrap().0;
+    
+    let menu = csv2im("./config/ramlist/menu.csv").unwrap();
+    context.insert("menu", &menu);
+    
+    match TEMPLATES.render("rl_menu.html", &context) {
+        Ok(body) => Ok(HttpResponse::Ok().body(body)),
+        Err(err) => {
+            eprintln!("Tera error: {}", err.source().unwrap().clone());
+            Err(error::ErrorInternalServerError(err))
+        }, 
+    }
+}
+
+// RAMList non-menu pages
+async fn ramlist(list: Path<Info>) -> impl Responder {
+    let mut lists = Vec::new();
+    // Only add to "lists" what is a list page
+    for entry in csv2im("./config/ramlist/menu.csv").unwrap() {
+        if entry["type"] == "list" {
+            lists.push(entry["name"].clone());
+        }
+    }
+    
+    // Other content page: about
+    if list.page == "about" {
+        let result = mkcontext("ramlist", "about").unwrap();
+        let mut context = result.0;
+        let index = result.1;
+        
+        let mut content = md2html(&index["content"]).unwrap();
+        
+        // Hacky solution until I find out how to have Comrak put the class there
+        content = content.replace("<h2>", "<h2 class=\"nopix\">");
+        
+        context.insert("content", &content);
+        
+        match TEMPLATES.render("rl_about.html", &context) {
+            Ok(body) => Ok(HttpResponse::Ok().body(body)),
+            Err(err) => {
+                eprintln!("Tera error: {}", err.source().unwrap());
+                Err(error::ErrorInternalServerError(err))
+            },
+        }
+    // Other content page: announcements
+    } else if list.page == "log" {
+        let mut context = mkcontext("ramlist", "log").unwrap().0;
+        
+        let entries = csv2im("./config/ramlist/log/posts.csv").unwrap();
+        
+        let mut content = Vec::new();
+        for entry in entries {
+            let mut tmpim = BTreeMap::new();
+            tmpim.insert("content", md2html(&format!("./config/ramlist/log/{}", entry["path"].clone())).unwrap());
+            tmpim.insert("date", entry["date"].clone());
+            content.push(tmpim); 
+        }
+        
+        context.insert("content", &content);
+        
+        match TEMPLATES.render("rl_log.html", &context) {
+            Ok(body) => Ok(HttpResponse::Ok().body(body)),
+            Err(err) => {
+                eprintln!("Tera error: {}", err.source().unwrap());
+                Err(error::ErrorInternalServerError(err))
+            },
+        }
+    // List pages
+    // Test if the page is defined in menu.csv
+    } else if lists.contains(&list.page) {
+        let mut context = mkcontext("ramlist", &list.page).unwrap().0;
+        
+        let content = rl_list_gen(&list.page).unwrap();
+        let tables = &content.0;
+        let headers = &content.1;
+        
+        context.insert("tables", &tables);
+        context.insert("headers", &headers);
+        context.insert("entries", &content.2);
+        
+        // Get vec of table keys; vendor names
+        let mut tables_keys = Vec::new();
+        for (key, _value) in tables.iter() {
+            tables_keys.push(key);
+        }
+        context.insert("tables_keys", &tables_keys);
+        
+        // Get vec of header keys
+        let mut headers_keys = Vec::new();
+        for (key, _value) in headers.iter() {
+            headers_keys.push(key);
+        }
+        context.insert("headers_keys", &headers_keys);
+        
+        // Width may be defined by an external config file later on
+        context.insert("table_width", "2750pt");
+        
+        match TEMPLATES.render("rl_list.html", &context) {
+            Ok(body) => Ok(HttpResponse::Ok().body(body)),
+            Err(err) => {
+                eprintln!("Tera error: {}", err.source().unwrap());
+                Err(error::ErrorInternalServerError(err))
+            },
+        }
+    } else {
+        let context = mkcontext("error", "404").unwrap().0;
+        
+        match TEMPLATES.render("err_404.html", &context) {
+            Ok(body) => Ok(HttpResponse::build(StatusCode::NOT_FOUND).content_type("text/html; charset=utf-8").body(body)),
+            Err(err) => {
+                eprintln!("Tera error: {}", err.source().unwrap());
+                Err(error::ErrorInternalServerError(err))
+            }, 
+        }
+    }
+}
     
 // Blog post list
 async fn blog_main() -> impl Responder {
@@ -123,6 +242,8 @@ async fn main() -> std::io::Result<()> {
             // TODO: figure out how to redirect to page with / at the end
             .service(fs::Files::new("/static/", "./static/")
                 .use_last_modified(true))
+            .route("/ramlist/", web::get().to(rl_main))
+            .route("/ramlist/{page}/", web::get().to(ramlist))
             .route("/blog/", web::get().to(blog_main))
             .route("/blog/{page}/", web::get().to(blog_post))
             .route("/privacy/", web::get().to(privacy))
